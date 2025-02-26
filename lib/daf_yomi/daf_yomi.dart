8f93ee5900ce1f3ab1044eaf5ecf73392fd7e7b7
Sivan Ratson <89018301+Sivan22@users.noreply.github.com>
2025-02-26 14:55:46 +0200

    feat: add tooltip to daf yomi opening

--- lib/daf_yomi/daf_yomi.dart
+++ lib/daf_yomi/daf_yomi.dart
@@ -18,51 +18,54 @@ class DafYomi extends StatelessWidget {
 
         final tractate = dafYomi.getMasechta();
         final dafAmud = dafYomi.getDaf();
-        return InkWell(
-          onTap: () => onDafYomiTap(
-            tractate,
-            formatAmud(dafAmud),
-          ),
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.surface.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(8),
+        return Tooltip(
+          message: 'פתח דף יומי',
+          child: InkWell(
+            onTap: () => onDafYomiTap(
+              tractate,
+              formatAmud(dafAmud),
             ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      getHebrewDateFormattedAsString(DateTime.now()),
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary,
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        getHebrewDateFormattedAsString(DateTime.now()),
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textDirection: TextDirection.rtl,
                       ),
-                      textDirection: TextDirection.rtl,
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      'דף היומי: $tractate ${formatAmud(dafAmud)}',
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.secondary,
-                        fontSize: 11,
+                      const SizedBox(height: 4),
+                      Text(
+                        'דף היומי: $tractate ${formatAmud(dafAmud)}',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontSize: 11,
+                        ),
+                        textDirection: TextDirection.rtl,
                       ),
-                      textDirection: TextDirection.rtl,
-                    ),
-                  ],
-                ),
-                const SizedBox(width: 10),
-                Icon(
-                  Icons.calendar_month_outlined,
-                  color: Theme.of(context).colorScheme.secondary,
-                  size: 24,
-                ),
-              ],
+                    ],
+                  ),
+                  const SizedBox(width: 10),
+                  Icon(
+                    Icons.calendar_month_outlined,
+                    color: Theme.of(context).colorScheme.secondary,
+                    size: 24,
+                  ),
+                ],
+              ),
             ),
           ),
         );
