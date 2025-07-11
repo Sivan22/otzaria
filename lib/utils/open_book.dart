2d6320f7012d6b19141d75f1e396a19db6321e23
Y-PLONI <7353755@gmail.com>
2025-07-11 19:32:48 +0300

    תיקון שגיאה קטנה

--- lib/utils/open_book.dart
+++ lib/utils/open_book.dart
@@ -11,21 +11,27 @@ import 'package:otzaria/tabs/models/text_tab.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
 void openBook(BuildContext context, Book book, int index, String searchQuery) {
+  // שלב 1: חישוב הערך הבוליאני ושמירתו במשתנה נפרד
+  // זה הופך את הקוד לקריא יותר ומונע את השגיאה
+  final bool shouldOpenLeftPane =
+      (Settings.getValue<bool>('key-pin-sidebar') ?? false) ||
+      (Settings.getValue<bool>('key-default-sidebar-open') ?? false);
+
+  // שלב 2: שימוש במשתנה החדש בשני המקרים
   if (book is TextBook) {
     context.read<TabsBloc>().add(AddTab(TextBookTab(
-        book: book,
-        index: index,
-        searchText: searchQuery,
-        openLeftPane: (Settings.getValue<bool>('key-pin-sidebar') ?? false) ||
-            (Settings.getValue<bool>('key-default-sidebar-open') ?? false)));
+          book: book,
+          index: index,
+          searchText: searchQuery,
+          openLeftPane: shouldOpenLeftPane, // שימוש במשתנה הפשוט
+        )));
   } else if (book is PdfBook) {
     context.read<TabsBloc>().add(AddTab(PdfBookTab(
           book: book,
           pageNumber: index,
-          openLeftPane:
-              (Settings.getValue<bool>('key-pin-sidebar') ?? false) ||
-                  (Settings.getValue<bool>('key-default-sidebar-open') ?? false),
+          openLeftPane: shouldOpenLeftPane, // שימוש באותו משתנה פשוט
         )));
   }
+
   context.read<NavigationBloc>().add(const NavigateToScreen(Screen.reading));
 }
