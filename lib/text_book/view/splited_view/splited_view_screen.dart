283b475983aa235e43a9027ae07715be9ca9b9e5
Y-PLONI <7353755@gmail.com>
2025-07-22 08:49:37 +0300

    לחצן לסגירת ופתיחת חלונית הפרשנים

--- lib/text_book/view/splited_view/splited_view_screen.dart
+++ lib/text_book/view/splited_view/splited_view_screen.dart
@@ -34,13 +34,42 @@ class _SplitedViewScreenState extends State<SplitedViewScreen> {
   static final GlobalKey<SelectionAreaState> _selectionKey =
       GlobalKey<SelectionAreaState>();
 
+  bool _paneOpen = true;
+
   @override
   void initState() {
     super.initState();
-    _controller = MultiSplitViewController(areas: [
-      Area(weight: 0.4, minimalSize: 200),
-      Area(weight: 0.6, minimalSize: 200),
-    ]);
+    _controller = MultiSplitViewController(areas: _openAreas());
+  }
+
+  List<Area> _openAreas() => [
+        Area(weight: 0.4, minimalSize: 200),
+        Area(weight: 0.6, minimalSize: 200),
+      ];
+
+  List<Area> _closedAreas() => [
+        Area(weight: 0, minimalSize: 0),
+        Area(weight: 1, minimalSize: 200),
+      ];
+
+  void _updateAreas() {
+    _controller.areas = _paneOpen ? _openAreas() : _closedAreas();
+  }
+
+  void _togglePane() {
+    setState(() {
+      _paneOpen = !_paneOpen;
+      _updateAreas();
+    });
+  }
+
+  void _openPane() {
+    if (!_paneOpen) {
+      setState(() {
+        _paneOpen = true;
+        _updateAreas();
+      });
+    }
   }
 
   @override
@@ -67,7 +96,17 @@ class _SplitedViewScreenState extends State<SplitedViewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TextBookBloc, TextBookState>(
+    return BlocConsumer<TextBookBloc, TextBookState>(
+      listenWhen: (previous, current) {
+        return previous is TextBookLoaded &&
+            current is TextBookLoaded &&
+            previous.activeCommentators != current.activeCommentators;
+      },
+      listener: (context, state) {
+        if (state is TextBookLoaded) {
+          _openPane();
+        }
+      },
       buildWhen: (previous, current) {
         if (previous is TextBookLoaded && current is TextBookLoaded) {
           return previous.fontSize != current.fontSize ||
@@ -80,49 +119,65 @@ class _SplitedViewScreenState extends State<SplitedViewScreen> {
         if (state is! TextBookLoaded) {
           return const Center(child: CircularProgressIndicator());
         }
-
-        return MultiSplitView(
-          controller: _controller,
-          axis: Axis.horizontal,
-          resizable: true,
-          dividerBuilder:
-              (axis, index, resizable, dragging, highlighted, themeData) {
-            final color = dragging
-                ? Theme.of(context).colorScheme.primary
-                : Theme.of(context).dividerColor;
-            return MouseRegion(
-              cursor: SystemMouseCursors.resizeColumn,
-              child: Container(
-                width: 8,
-                alignment: Alignment.center,
-                child: Container(
-                  width: 1.5,
-                  color: color,
-                ),
-              ),
-            );
-          },
+        return Stack(
           children: [
-            ContextMenuRegion(
-              contextMenu: _buildContextMenu(state),
-              child: SelectionArea(
-                key: _selectionKey,
-                child: CommentaryList(
-                  index: 0,
-                  fontSize: state.fontSize,
-                  openBookCallback: widget.openBookCallback,
-                  showSplitView: state.showSplitView,
+            MultiSplitView(
+              controller: _controller,
+              axis: Axis.horizontal,
+              resizable: true,
+              dividerBuilder:
+                  (axis, index, resizable, dragging, highlighted, themeData) {
+                final color = dragging
+                    ? Theme.of(context).colorScheme.primary
+                    : Theme.of(context).dividerColor;
+                return MouseRegion(
+                  cursor: SystemMouseCursors.resizeColumn,
+                  child: Container(
+                    width: 8,
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 1.5,
+                      color: color,
+                    ),
+                  ),
+                );
+              },
+              children: [
+                ContextMenuRegion(
+                  contextMenu: _buildContextMenu(state),
+                  child: SelectionArea(
+                    key: _selectionKey,
+                    child: _paneOpen
+                        ? CommentaryList(
+                            index: 0,
+                            fontSize: state.fontSize,
+                            openBookCallback: widget.openBookCallback,
+                            showSplitView: state.showSplitView,
+                          )
+                        : const SizedBox.shrink(),
+                  ),
                 ),
+                SimpleBookView(
+                  data: widget.content,
+                  textSize: state.fontSize,
+                  openBookCallback: widget.openBookCallback,
+                  openLeftPaneTab: widget.openLeftPaneTab,
+                  showSplitedView: state.showSplitView,
+                  tab: widget.tab,
+                ),
+              ],
+            ),
+            Positioned(
+              left: 0,
+              top: 0,
+              child: IconButton(
+                iconSize: 16,
+                padding: EdgeInsets.zero,
+                visualDensity: VisualDensity.compact,
+                icon: Icon(_paneOpen ? Icons.close : Icons.drag_handle),
+                onPressed: _togglePane,
               ),
-            ),
-            SimpleBookView(
-              data: widget.content,
-              textSize: state.fontSize,
-              openBookCallback: widget.openBookCallback,
-              openLeftPaneTab: widget.openLeftPaneTab,
-              showSplitedView: state.showSplitView,
-              tab: widget.tab,
-            ),
+            )
           ],
         );
       },
